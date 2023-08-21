4095fc60d3dd7005cdf0f9234f310875a1660958
Jonathan Ellis <jbellis@datastax.com>
2023-08-21 09:56:23 -0500

    update for new output format

--- plot_output.py
+++ plot_output.py
@@ -1,93 +1,87 @@
 import sys
-import re
 import matplotlib.pyplot as plt
 
-# Function to parse data and extract relevant information
-def parse_data(description, data):
-    # Extract base vector count and query vector count from description
-    base_vector_count = int(re.search(r'(\d+) base', description).group(1))
-    query_vector_count = int(re.search(r'(\d+) query', description).group(1))
-    dimensions = int(re.search(r'dimensions (\d+)', description).group(1))
-    dataset_name = re.search(r'hdf5/(\S+).hdf5', description).group(1)
+def parse_file(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
 
-    parsed_data = []
-    for line in data:
-        # Extract recall, query time, M and ef values
-        recall = float(re.search(r'recall (\d+\.\d+)', line).group(1))
-        query_time = float(re.search(r'query (\d+\.\d+)s', line).group(1))
-        M = int(re.search(r'M=(\d+)', line).group(1))
-        ef = int(re.search(r'ef=(\d+)', line).group(1))
-        overquery = int(re.search(r'top 100/(\d+) ', line).group(1))
-        
-        # Calculate throughput
-        throughput = query_vector_count * 10 / query_time
-        
-        parsed_data.append((recall, throughput, M, ef, overquery))
-    
-    return {
-        'name': dataset_name,
-        'base_vector_count': base_vector_count,
-        'dimensions': dimensions,
-        'data': parsed_data
-    }
+    datasets = []
+    current_dataset = None
+    current_M = None
+    current_ef = None
 
-def is_pareto_optimal(candidate, others):
-    """Determine if a candidate point is Pareto-optimal."""
-    for point in others:
-        # Check if another point has higher or equal recall and throughput
-        if point[0] >= candidate[0] and point[1] > candidate[1]:
-            return False
-        if point[0] > candidate[0] and point[1] >= candidate[1]:
-            return False
+    for line in lines:
+        line = line.strip()
+        
+        if 'dimensions' in line:
+            if current_dataset:
+                datasets.append(current_dataset)
+            base_count = int(line.split(":")[1].split("base")[0].strip())
+            query_count = int(line.split("and")[1].split("query")[0].strip())
+            current_dataset = {
+                "title": line.split(":")[0].split("/")[-1].split(".")[0],
+                "base_count": base_count,
+                "query_count": query_count,
+                "queries": []
+            }
+        elif line.startswith("HNSW"):
+            if "build in" in line:
+                current_M = int(line.split("M=")[1].split()[0])
+                current_ef = int(line.split("ef=")[1].split()[0])
+            else:
+                OQ = int(line.split("top 100/")[1].split()[0])
+                recall = float(line.split("recall ")[1].split(",")[0])
+                query_time = float(line.split("query ")[1].split("s.")[0])
+                throughput = (2 * current_dataset["query_count"]) / query_time
+                
+                current_dataset["queries"].append({
+                    "OQ": OQ,
+                    "recall": recall,
+                    "throughput": throughput,
+                    "M": current_M,
+                    "ef": current_ef
+                })
+
+    datasets.append(current_dataset)
+    return datasets
+
+def is_pareto_optimal(current_point, other_points):
+    for other_point in other_points:
+        if other_point["recall"] >= current_point["recall"] and other_point["throughput"] >= current_point["throughput"]:
+            if other_point["recall"] > current_point["recall"] or other_point["throughput"] > current_point["throughput"]:
+                return False
     return True
 
-def filter_pareto_optimal(data):
-    """Filter out only the Pareto-optimal points."""
-    return [point for point in data if is_pareto_optimal(point, data)]
+def plot_dataset(dataset, save_as_png=False):
+    plt.figure(figsize=(12, 8))
+    
+    recalls = [query["recall"] for query in dataset["pareto_queries"]]
+    throughputs = [query["throughput"] for query in dataset["pareto_queries"]]
+    annotations = [f"OQ={query['OQ']} M={query['M']} ef={query['ef']}" for query in dataset["pareto_queries"]]
 
-def plot_dataset(dataset, output_dir="."):
-    # Extract dataset info
-    name = dataset['name']
-    base_vector_count = dataset['base_vector_count']
-    dimensions = dataset['dimensions']
-    data = dataset['data']
-    
-    # Create plot
-    plt.figure(figsize=(15, 20))
-    for recall, throughput, M, ef, overquery in data:
-        plt.scatter(recall, throughput, label=f'M={M}, ef={ef}, oq={overquery}')
-        plt.annotate(f'M={M}, ef={ef}, oq={overquery}', (recall, throughput))
-    
-    # Set title and labels
-    plt.title(f"Dataset: {name}\\nBase Vector Count: {base_vector_count}\\nDimensions: {dimensions}")
-    plt.xlabel('Recall')
-    plt.ylabel('Throughput')
-    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.scatter(recalls, throughputs, marker='o', c='blue')
+    for i, txt in enumerate(annotations):
+        plt.annotate(txt, (recalls[i], throughputs[i]), fontsize=9, ha='right')
+
+    plt.xlabel("Recall")
+    plt.ylabel("Throughput")
+    plt.title(dataset["title"])
     plt.grid(True, which='both', linestyle='--', linewidth=0.5)
     plt.tight_layout()
-    
-    # Save the plot to a file
-    filename = f"{output_dir}/{name}_plot.png"
-    plt.savefig(filename)
-    print("saved " + filename)
-    
-    # Clear the figure for the next plot
-    plt.clf()
 
-# Load and parse data
-with open(sys.argv[1], 'r') as file:
-    content = file.read().strip().split('\n\n')
-datasets = []
-for dataset in content:
-    lines = dataset.split('\n')
-    description = lines[0]
-    data = lines[1:]
-    datasets.append((description, data))
-parsed_datasets = [parse_data(desc, data) for desc, data in datasets]
+    if save_as_png:
+        fname = dataset["title"] + ".png"
+        print("saved " + fname)
+        plt.savefig(fname)
+    else:
+        plt.show()
 
-# Filter and plot
-for dataset in parsed_datasets:
-    dataset['data'] = filter_pareto_optimal(dataset['data'])
-for dataset in parsed_datasets:
-    plot_dataset(dataset)
+def main():
+    filename = sys.argv[1]
+    datasets = parse_file(filename)
+    for dataset in datasets:
+        dataset["pareto_queries"] = [query for query in dataset["queries"] if is_pareto_optimal(query, dataset["queries"])]
+        plot_dataset(dataset, save_as_png=True)
 
+if __name__ == "__main__":
+    main()
