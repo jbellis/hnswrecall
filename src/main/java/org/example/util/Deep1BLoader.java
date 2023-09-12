b5c01f6f74bedd276b5bf9a3a9d3539df6ded087
Jonathan Ellis <jbellis@datastax.com>
2023-09-12 11:36:35 -0500

    simplify multithreaded load

--- src/main/java/org/example/util/Deep1BLoader.java
+++ src/main/java/org/example/util/Deep1BLoader.java
@@ -10,7 +10,6 @@ import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 public class Deep1BLoader {
@@ -29,30 +28,27 @@ public class Deep1BLoader {
         int perThread = count / threadCount;
         int remainder = count % threadCount;
 
-        ForkJoinPool customThreadPool = new ForkJoinPool(threadCount);
-        customThreadPool.submit(() -> {
-            IntStream.range(0, threadCount).parallel().forEach(threadNum -> {
-                try (var raf = new RandomAccessFile(filePath, "r")) {
-                    long startPosition = 8L + ((long) threadNum * perThread + Math.min(threadNum, remainder)) * dimension * Float.BYTES;
-                    raf.seek(startPosition);
+        IntStream.range(0, threadCount).parallel().forEach(threadNum -> {
+            try (var raf = new RandomAccessFile(filePath, "r")) {
+                long startPosition = 8L + ((long) threadNum * perThread + Math.min(threadNum, remainder)) * dimension * Float.BYTES;
+                raf.seek(startPosition);
 
-                    int itemsToProcess = perThread + (threadNum < remainder ? 1 : 0);
-                    for (int j = 0; j < itemsToProcess; j++) {
-                        var buffer = new byte[dimension * Float.BYTES];
-                        raf.readFully(buffer);
-                        var byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN);
+                int itemsToProcess = perThread + (threadNum < remainder ? 1 : 0);
+                for (int j = 0; j < itemsToProcess; j++) {
+                    var buffer = new byte[dimension * Float.BYTES];
+                    raf.readFully(buffer);
+                    var byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN);
 
-                        var vector = new float[dimension];
-                        var floatBuffer = byteBuffer.asFloatBuffer();
-                        floatBuffer.get(vector);
-                        vectors[threadNum * perThread + (Math.min(threadNum, remainder)) + j] = vector;
-                    }
+                    var vector = new float[dimension];
+                    var floatBuffer = byteBuffer.asFloatBuffer();
+                    floatBuffer.get(vector);
+                    vectors[threadNum * perThread + (Math.min(threadNum, remainder)) + j] = vector;
                 }
-                catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
-            });
-        }).join();
+            }
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
 
         System.out.printf("Completed reading %s%n".formatted(filePath));
         return List.of(vectors);
