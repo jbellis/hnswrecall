c50a04e25f0085816138a7d4fbe86494aa9f177a
Jonathan Ellis <jbellis@datastax.com>
2023-09-11 23:54:52 -0500

    multithreaded load

--- src/main/java/org/example/util/Deep1BLoader.java
+++ src/main/java/org/example/util/Deep1BLoader.java
@@ -1,54 +1,83 @@
 package org.example.util;
 
-import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
 
 public class Deep1BLoader {
-    public static float[][] readFBin(String filePath, int count) throws IOException {
+    public static List<float[]> readFBin(String filePath, int count) throws IOException {
         var vectors = new float[count][];
-        try (var dis = new DataInputStream(new BufferedInputStream(new FileInputStream(filePath)))) {
-            int n = Integer.reverseBytes(dis.readInt());
-            System.out.printf("File contains %d vectors; reading %d%n", n, count);
-            var dimension = Integer.reverseBytes(dis.readInt());
-            for (int i = 0; i < count; i++) {
-                var buffer = new byte[dimension * Float.BYTES];
-                dis.readFully(buffer);
-                var byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN);
 
-                var vector = new float[dimension];
-                var floatBuffer = byteBuffer.asFloatBuffer();
-                floatBuffer.get(vector);
-                vectors[i] = (vector);
-            }
+        int n;
+        int dimension;
+        try (var raf = new RandomAccessFile(filePath, "r")) {
+            n = Integer.reverseBytes(raf.readInt());
+            dimension = Integer.reverseBytes(raf.readInt());
         }
-        return vectors;
+        System.out.printf("%s contains %d vectors; reading %d%n", filePath, n, count);
+
+        var threadCount = 16;
+        int perThread = count / threadCount;
+        int remainder = count % threadCount;
+
+        ForkJoinPool customThreadPool = new ForkJoinPool(threadCount);
+        customThreadPool.submit(() -> {
+            IntStream.range(0, threadCount).parallel().forEach(threadNum -> {
+                try (var raf = new RandomAccessFile(filePath, "r")) {
+                    long startPosition = 8L + ((long) threadNum * perThread + Math.min(threadNum, remainder)) * dimension * Float.BYTES;
+                    raf.seek(startPosition);
+
+                    int itemsToProcess = perThread + (threadNum < remainder ? 1 : 0);
+                    for (int j = 0; j < itemsToProcess; j++) {
+                        var buffer = new byte[dimension * Float.BYTES];
+                        raf.readFully(buffer);
+                        var byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN);
+
+                        var vector = new float[dimension];
+                        var floatBuffer = byteBuffer.asFloatBuffer();
+                        floatBuffer.get(vector);
+                        vectors[threadNum * perThread + (Math.min(threadNum, remainder)) + j] = vector;
+                    }
+                }
+                catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+        }).join();
+
+        System.out.printf("Completed reading %s%n".formatted(filePath));
+        return List.of(vectors);
     }
 
-    public static ArrayList<HashSet<Integer>> readIvecs(String filename) {
+    public static ArrayList<HashSet<Integer>> readGT(String filePath) {
         var groundTruthTopK = new ArrayList<HashSet<Integer>>();
 
-        try (var dis = new DataInputStream(new FileInputStream(filename))) {
-            while (dis.available() > 0) {
-                var numNeighbors = Integer.reverseBytes(dis.readInt());
-                var neighbors = new HashSet<Integer>(numNeighbors);
-
-                for (var i = 0; i < numNeighbors; i++) {
+        try (var dis = new DataInputStream(new FileInputStream(filePath))) {
+            var n = Integer.reverseBytes(dis.readInt());
+            var topK = Integer.reverseBytes(dis.readInt());
+            for (int i = 0; i < n; i++) {
+                var neighbors = new HashSet<Integer>(topK);
+                for (var j = 0; j < topK; j++) {
                     var neighbor = Integer.reverseBytes(dis.readInt());
                     neighbors.add(neighbor);
                 }
-
                 groundTruthTopK.add(neighbors);
             }
+            // GT file also contains scores, we don't need those
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new UncheckedIOException(e);
         }
 
+        System.out.printf("Completed reading %s%n".formatted(filePath));
         return groundTruthTopK;
     }
 }
